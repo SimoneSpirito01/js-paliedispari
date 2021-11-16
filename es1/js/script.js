ece336d4385d29d32cdcbe905ad8b01576b2b13f
SimoneSpirito <simospiri01@gmail.com>
2021-11-16 13:48:25 +0100

    es1

--- es1/js/script.js
+++ es1/js/script.js
@@ -1,2 +1,33 @@
 
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+let word = prompt('Inserisci una parola');
+
+while (!isNaN(word)){
+    word = prompt('ATTENZIONE! Inserisci una parola');
+}
+
+word = word.toLocaleLowerCase();
+
+const palindroma = pal(word);
+let x;
+
+if (palindroma){
+    x = 'La parola che hai inserito è palindroma'
+} else {
+    x = 'La parola che hai inserito non è palindroma'
+}
+
+document.querySelector('.check').innerHTML = x;
+
+function pal(parola){
+    const revWord = [];
+    for (let i = parola.length - 1; i >= 0; i--){
+        revWord.push(parola[i]);
+        console.log(revWord);
+    }
+    if (parola == revWord.join('')){
+        return true;
+    }
+    return false;
+}
